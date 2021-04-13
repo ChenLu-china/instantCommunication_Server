759c627a275beb3f668b705de78a69aae3fec962
carlosouyang <carlosouyang@outlook.com>
2021-04-13 12:07:27 +0800

    修改bind ip

--- test/simple_client.py
+++ test/simple_client.py
@@ -3,14 +3,13 @@ import socket
 import time
 
 MaxBytes = 1024 * 1024
-host = '127.0.0.1'
+host = '159.75.220.96'
 port = 8888
 
 s = socket.socket(socket.AF_INET,
                   socket.SOCK_STREAM)
 
 s.connect((host, port))
-time.sleep(5)
 msg1 = {'head': 'login', 'user_name': "ChenLu", 'user_password': "cl961007"}
 jmsg1 = json.dumps(msg1)
 s.send(jmsg1.encode('utf-8'))
