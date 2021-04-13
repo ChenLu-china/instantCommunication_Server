7ff4e5436852720898d74fb08eb0795e462f9225
carlosouyang <carlosouyang@outlook.com>
2021-04-14 01:52:17 +0800

    修改地址

--- dao/GeneralDao.py
+++ dao/GeneralDao.py
@@ -1,9 +1,9 @@
 import pymysql
 
-# host = '172.16.0.8'
-# port = 3306
-host = 'gz-cdb-7fa8sxrh.sql.tencentcdb.com'
-port = 58915
+host = '172.16.0.8'
+port = 3306
+# host = 'gz-cdb-7fa8sxrh.sql.tencentcdb.com'
+# port = 58915
 
 def create_MySql_Connection():
     conn = pymysql.connect(
--- test/udp_client.py
+++ test/udp_client.py
@@ -1,6 +1,7 @@
 import socket
 import json
 
+# host = '159.75.220.96'
 target_ip = '127.0.0.1'
 target_port = 7777
 
